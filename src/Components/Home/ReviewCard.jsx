c3960297d269d745faf07a3c79ba075c4b827175
TH-Raju <rajukhan894200@gmail.com>
2023-03-20 17:57:02 +0600

    Home page design complete

---
+++ src/Components/Home/ReviewCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import quort from '../../assets/icons/quote.svg'
+
+const ReviewCard = ({ img, name, title, text }) => {
+    return (
+        <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md  ">
+            <div className="flex justify-between p-4">
+                <div className="flex space-x-4">
+                    <div>
+                        <img src={img} alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                    </div>
+                    <div>
+                        <h4 className="font-bold">{name}</h4>
+                        <span className="text-xs ">{title}</span>
+                    </div>
+                </div>
+                <div className="flex items-center space-x-2 dark:text-yellow-500">
+                    <img src={quort} alt="quote" className='w-10 h-10 opacity-25' />
+                </div>
+            </div>
+            <div className="p-4 space-y-2 text-sm ">
+                <p>{text}</p>
+            </div>
+            <div className="rating ml-4">
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400 " />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400 " />
+            </div>
+        </div>
+    );
+};
+
+export default ReviewCard;
--- src/Components/Home/Testimonial.jsx
+++ src/Components/Home/Testimonial.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import ReviewCard from './ReviewCard';
+import img1 from '../../assets/images/testimonial/player-1.png'
+import img2 from '../../assets/images/testimonial/player-2.png'
+import img3 from '../../assets/images/testimonial/player-3.png'
+import img4 from '../../assets/images/testimonial/player-4.png'
 
 const Testimonial = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
     return (
         <div className='my-20'>
             <div className='text-center mb-8'>
                 <p className="text-2xl font-bold text-orange-600">Testimonial</p>
                 <h2 className='text-5xl font-semibold my-6'>What Customer Says</h2>
                 <p className='w-1/2 mx-auto'>The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly Believable.</p>
+            </div>
+            <div>
+                <Carousel responsive={responsive} className="mx-10 mt-5">
+                    <div >
+                        <ReviewCard img={img1} name={'Leo jekkins'} title={"Businessman"} text={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "}></ReviewCard>
+                    </div>
+                    <div>
+                        <ReviewCard img={img2} name={'Jr. Smith'} title={"Engineer"} text={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "}></ReviewCard>
+                    </div>
+                    <div>
+                        <ReviewCard img={img3} name={'John Henry'} title={"Lawyer"} text={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "}></ReviewCard>
+                    </div>
+                    <div>
+                        <ReviewCard img={img4} name={'Emi Andreson'} title={"Doctor"} text={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "}></ReviewCard>
+                    </div>
+                </Carousel>
             </div>
         </div>
     );
