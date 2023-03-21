fa53f9b84da183e1666bc29319873c5c503d1fc6
TH-Raju <rajukhan894200@gmail.com>
2023-03-21 16:44:34 +0600

    create user with firebase complete

--- src/Components/LogSign/SignUp.jsx
+++ src/Components/LogSign/SignUp.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import login from '../../assets/images/login/login.svg';
+import { AuthContext } from '../../context/AuthProvider';
 
 const SignUp = () => {
+
+    const { createUser } = useContext(AuthContext)
+
     const handleSignUp = event => {
         event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(err => console.log(err))
 
     }
 
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,9 +1,9 @@
-import React, { createContext, useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 const auth = getAuth(app);
 
 
@@ -11,9 +11,29 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser);
+            setUser(currentUser)
+        });
+
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+
+
     const authInfo = {
         user,
         loading,
+        createUser
     }
     return (
         <AuthContext.Provider value={authInfo}>
