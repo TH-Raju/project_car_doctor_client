e2d6bd452a4858c688825b875744df8609c6b47f
TH-Raju <rajukhan894200@gmail.com>
2023-03-30 17:21:26 +0600

    connect to the server

--- src/Components/Home/Products.jsx
+++ src/Components/Home/Products.jsx
@@ -5,7 +5,7 @@ const Products = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('product.json')
+        fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
